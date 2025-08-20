2d191bbe724e102094935c81a01bc401bf70e754
Hannah Görigk <hannah.goerigk@eonerc.rwth-aachen.de>
2025-08-20 12:18:58 +0200

    started adding docstrings to config.py

--- districtgenerator/data_handling/config.py
+++ districtgenerator/data_handling/config.py
@@ -1,19 +1,28 @@
-from dataclasses import dataclass, field
+from dataclasses import field
 import os
-from typing import Any, Dict, List, ClassVar, Set, Optional
+from typing import ClassVar, Set, Optional
 from pydantic import BaseModel, ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from districtgenerator.data_handling.central_device_config import CentralDeviceConfig
 from districtgenerator.data_handling.decentral_device_config import DecentralDeviceConfig
-from pathlib import Path
 
 
 class LocationConfig(BaseSettings):
-    timeZone: float = 1 #1
-    albedo: float = 0.2
-    TRYYear: str = 'TRY2015'
-    TRYType: str = 'Jahr'
-    zip: str = '52062'
+    """
+    LocationConfig class to manage location-related parameters for the district generator.
+    This class contains parameters related to the geographical location, time zone, albedo,
+    and TRY (Test Reference Year) data used in the district generator.
+
+    Attributes
+    ----------
+    Descriptions in Class.
+
+    """
+    timeZone: float = 1         # Shift between the location's time and GMT in hours. CET would be 1.
+    albedo: float = 0.2         # Ground reflectance. 0 refers to 0% and 1 refers to 100%.
+    TRYYear: str = 'TRY2015'    # Test reference year of DWD. Possible entries are TRY2015 and TRY2045.
+    TRYType: str = 'Jahr'       # Test reference conditions of DWD. Possible entries are Jahr, Somm, Wint.
+    zip: str = '52062'          # Zip code of the location.
 
     ALLOWED_TRY_YEARS: ClassVar[Set[str]] = {"TRY2015", "TRY2045"}
     ALLOWED_TRY_TYPES: ClassVar[Set[str]] ={"Jahr", "Somm", "Wint"}
@@ -31,84 +40,175 @@ class LocationConfig(BaseSettings):
             raise ValueError("albedo must be between 0.0 and 1.0.")
 
 class TimeConfig(BaseSettings):
-    timeResolution: int = 3600
-    clusterLength: int = 604800
-    clusterNumber: int = 4
-    dataResolution: int = 3600
-    dataLength: int = 31536000
+    """
+    TimeConfig class to manage time-related parameters for the district generator.
+    This class contains parameters related to time resolution, cluster length, and holidays for different years.
+
+    Attributes
+    ----------
+    Descriptions in Class.
+
+    """
+    timeResolution: int = 3600  # Required time resolution in seconds. Tip: 3600 refers to an hourly resolution. 900 to a 15min resolution.
+    clusterLength: int = 604800 # Length of cluster. Tip: 604800 refers to one week. 86400 for one day.
+    clusterNumber: int = 4      # Number of clusters
+    dataResolution: int = 3600  # Time resolution of input data in seconds. (If you don't change weather data, here is no need to change).
+    dataLength: int = 31536000  # Length of input data in seconds. (If you don't change weather data, here is no need to change).
+
     holidays2015: list = field(default_factory=lambda: [1, 93, 96, 121, 134, 145, 155, 275, 305, 358, 359, 360, 365],)
+        # Julian day number of the holidays in NRW in 2015.
     holidays2045: list = field(default_factory=lambda: [1, 97, 100, 121, 138, 149, 159, 276, 305, 358, 359, 360, 365],)
+        # Julian day number of the holidays in NRW in 2045.
     initial_day_2015: list = field(default_factory=lambda: [4])
+        # Thursday
     initial_day_2045: list = field(default_factory=lambda: [6])
+        # Saturday
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class DesignBuildingConfig(BaseSettings):
-    T_set_min: float = 20.0
-    T_set_min_night: float = 18.0
-    T_set_max: float = 23.0
-    T_set_max_night: float = 28.0
-    T_bivalent: float = -2.0
-    T_heatlimit: float = 15.0
-    ventilation_rate: float = 0.5
+    """
+    DesignBuildingConfig class to manage design parameters for all buildings in the district generator.
+    This class contains parameters related to building design, such as temperature settings, ventilation rates,
+    building types, retrofit options, and domestic hot water (DHW) load.
+
+    Attributes
+    ----------
+    Descriptions in Class.
+
+    """
+    T_set_min: float = 20.0         # Required minimum indoor temperature (for heating load calculation) in degrees Celsius
+    T_set_min_night: float = 18.0   # Required minimum indoor temperature at night (for heating load calculation) in degrees Celsius
+    T_set_max: float = 23.0         # Required maximum indoor temperature (for cooling load calculation) in degrees Celsius
+    T_set_max_night: float = 28.0   # Required maximum indoor temperature at night (for cooling load calculation) in degrees Celsius
+    T_bivalent: float = -2.0        # Dual mode temperature (for heat pump design) in degrees Celsius
+    T_heatlimit: float = 15.0       # Limit temperature (for heat pump design)
+    ventilation_rate: float = 0.5   # Room ventilation rate in 1/h (per hour)
+
     buildings_short: list = field(default_factory=lambda: ['SFH', 'MFH', 'TH', 'AB'])
+        # Abbreviations of the selectable building types.
     buildings_long: list = field(default_factory=lambda: ['single_family_house', 'multi_family_house', 'terraced_house', 'apartment_block'])
+        # Names of the four selectable building types.
     retrofit_short: list = field(default_factory=lambda: [0, 1, 2])
+        # Abbreviations of the retrofit levels.
     retrofit_long: list = field(default_factory=lambda: ['tabula_standard', 'tabula_retrofit', 'tabula_adv_retrofit'])
+        # Names of the retrofit levels.
     dhwload: list = field(default_factory=lambda: [4662.1, 4662.1, 4662.1, 3999.8])
+        # Maximal power for domestic hot water for each of the four building types (SFH, MFH, TH and AB)
     mean_drawoff_vol_per_day: list = field(default_factory=lambda: [40, 40, 40, 40])
+        # Mean drawoff DHW volume per day for each of the four building types (SFH, MFH, TH and AB). Source: 12831-3/A100 Table NA.4"
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class EcoConfig(BaseSettings):
-    price_supply_el: float = 0.32
-    revenue_feed_in_el: float = 0.0811
-    price_supply_gas: float = 0.12
-    price_hydrogen: float = 0.1
-    price_waste: float = 0.1
-    price_biomass: float = 0.05
-    co2_el_grid: float = 0.49
-    co2_gas: float = 0.25
-    co2_biom: float = 0.35
-    co2_waste: float = 0.0
-    co2_hydrogen: float = 0.0
+    """ EcoConfig class to manage economic parameters for the district generator.
+    This class contains parameters related to energy prices, CO2 emissions, and other economic factors
+    used in the district generator.
+
+    Attributes
+    ----------
+    Descriptions in Class.
+
+    """
+
+    price_supply_el: float = 0.32       # Electricity price in €/kWh
+    revenue_feed_in_el: float = 0.0811  # Feed-in electricity price in €/kWh
+    price_supply_gas: float = 0.12      # Gas price in €/kWh
+    price_hydrogen: float = 0.1         # Hydrogen price in €/kWh
+    price_waste: float = 0.1            # Waste price in €/kWh
+    price_biomass: float = 0.05         # Biomass price in €/kWh
+    co2_el_grid: float = 0.49           # Co2 emissions for electricity import (grid mix) in kg/kWh
+    co2_gas: float = 0.25               # Co2 emissions for burning natural gas in kg/kWh
+    co2_biom: float = 0.35              # Co2 emissions for burning biomass in kg/kWh
+    co2_waste: float = 0.0              # Co2 emissions for burning waste in kg/kWh
+    co2_hydrogen: float = 0.0           # Co2 emissions for burning hydrogen in kg/kWh
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class PhysicsConfig(BaseSettings):
-    rho_air: float = 1.2 # kg/m3
-    c_p_air: float = 1000.0
-    rho_water: float = 1000.0
-    c_p_water: float = 4.18
+    """
+    PhysicsConfig class to manage physical constants and parameters used in the district generator.
+
+    Attributes
+    ----------
+    rho_air : float
+        Density of air in kg/m3. Default is 1.2 kg/m3.
+    c_p_air : float
+        Specific heat capacity of air in J/(kg*K). Default is 1000.0 J/(kg*K).
+    rho_water : float
+        Density of water in kg/m3. Default is 1000.0 kg/m3.
+    c_p_water : float
+        Specific heat capacity of water in J/(kg*K). Default is 4.18 J/(kg*K).
+
+    """
+    rho_air: float = 1.2        # kg/m3
+    c_p_air: float = 1000.0     # J/(kg*K)
+    rho_water: float = 1000.0   # kg/m3
+    c_p_water: float = 4.18     # J/(kg*K)
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class GurobiConfig(BaseSettings):
+    """
+    GurobiConfig class to manage the configuration of the Gurobi optimization solver.
+
+    Attributes
+    ----------
+    Descriptions in Class.
+
+    """
     ModelName: str = "Central_Operational_Optimization" # Name of the model
-    TimeLimit: int = 3600         # seconds
-    MIPGap: float = 0.01
-    MIPFocus: int = 3 #
-    NonConvex: int = 2
-    NumericFocus: int = 3
-    PoolSolution: int = 3
-    DualReductions: int = 0
+    TimeLimit: int = 3600                               # The time limit for the optimization in seconds
+    MIPGap: float = 0.01                                # acceptable MIP gap, Default: 1%
+    MIPFocus: int = 3                                   # focus of the MIP solver, Default: 3 (balance between speed and solution quality)
+    NonConvex: int = 2                                  # non-convexity setting, Default: 2
+    NumericFocus: int = 3                               # numeric focus, Default: 3
+    PoolSolution: int = 3                               # number of solutions to pool, Default: 3
+    DualReductions: int = 0                             # level of dual reductions to apply, Default: 0
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class HeatGridConfig(BaseSettings):
+    """
+    HeatGridConfig class to manage the configuration of the heat grid in the district generator.
+    This class contains parameters related to the heat grid, such as flow and return temperatures,
+    temperature differences in heat exchangers, and other relevant settings.
+
+    Attributes
+    ----------
+    todo
+    """
     T_hot: float = 332.15           # Flow temperature in Kelvin
     T_cold: float = 323.15          # Return temperature in Kelvin
     delta_T_heatTransfer: float = 5 # Temperature difference in heat exchangers (K)
+
     model_config = SettingsConfigDict(
         extra="allow" 
     )
 
 class EHDOConfig(BaseSettings):
+    """
+    EHDOConfig class to manage the configuration of the Energy and Heat Distribution Optimization (EHDO) system.
+    This class contains parameters relevant for the central optimization of energy and heat distribution in the
+    district generator. It configures e.g. the use of certain technologies (electricity, gas, biomass, hydrogen, etc.)
+    and their respective prices, CO2 emissions, and supply limits.
+
+
+    Attributes
+    ----------
+    Descriptions in Class.
+    """
+
     # Electricity configuration
     enable_supply_el: bool = True
     enable_feed_in_el: bool = True
@@ -160,18 +260,62 @@ class EHDOConfig(BaseSettings):
     # Helper attributes for unit formatting
     unit_placeholder: str = " - "  # used for cases where unit is a placeholder
     unit_dash: str = "-"         # used for cases where unit is a dash
+
     model_config = SettingsConfigDict(
         extra="allow"
     )
 
 class scenarioName(BaseSettings):
-    scenario_name: str = 'base_scenario'
+    """
+    scenarioName class to manage the scenario name for the district generator.
+
+    Attributes
+    ----------
+    scenario_name : str
+        The name of the scenario being configured. Default is 'base_scenario'.
+        This can be used to identify the scenario in outputs and logs.
+
+    """
+    scenario_name: str = 'base_scenario' # default value for scenario name
+
     model_config = SettingsConfigDict(
         extra="allow"
     )
 
 
 class GlobalConfig(BaseModel):
+    """
+    GlobalConfig class to manage all configurations for the district generator.
+    This class aggregates all individual configuration classes and provides a unified interface
+    to access them. It is designed to be initialized with an environment file that contains
+    configuration parameters for each component.
+
+    Attributes
+    ----------
+    location : LocationConfig
+        Configuration parameters related to the location of the buildings.
+    time : TimeConfig
+        Configuration parameters related to time settings, such as time resolution and cluster length.
+    design_building : DesignBuildingConfig
+        Configuration parameters for design aspects of buildings in the district.
+    eco : EcoConfig
+        Economic parameters, including prices and CO2 emissions for various energy sources.
+    physics : PhysicsConfig
+        Physical constants and parameters used in the districtgenerator.
+    gurobi : GurobiConfig
+        Configuration parameters for the Gurobi optimization solver.
+    heatgrid : HeatGridConfig
+        Configuration parameters for the heat grid, including temperatures and heat transfer.
+    ehdo : EHDOConfig
+        Configuration parameters for the EHDO (Energy and Heat Distribution Optimization) system.
+    decentral : DecentralDeviceConfig
+        Configuration parameters for decentralized devices in the district.
+    central : CentralDeviceConfig
+        Configuration parameters for central devices in the district.
+    scenario_name : scenarioName
+        The name of the scenario being configured, used for identification and output purposes.
+
+    """
     location: 'LocationConfig'
     time: 'TimeConfig'
     design_building: 'DesignBuildingConfig'
@@ -185,15 +329,45 @@ class GlobalConfig(BaseModel):
     scenario_name: scenarioName
 
 class Settings(BaseSettings):
-    env_file: str = '.env.CONFIG'
+    """
+    Settings class to manage global configuration parameters.
+    This class is used to load configuration parameters from an environment file.
+
+    Attributes
+    ----------
+    env_file : str
+        The path to the environment file containing configuration parameters.
+    env_file_encoding : str
+        The encoding of the environment file, default is 'utf-8'.
+    extra : str
+        Specifies how to handle extra fields not defined in the model, default is 'allow'.
+
+    """
+    env_file: str = '.env.CONFIG.EXAMPLE'
 
     class Config:
-        env_file = '.env.CONFIG'  # Default .env file
+        env_file = '.env.CONFIG.EXAMPLE'  # Default .env file if no env_file is provided
         env_file_encoding = 'utf-8'
         extra = 'allow'
 
 
 def load_global_config(env_file: Optional[str] = None) -> GlobalConfig:
+    """
+    Load the global configuration from the specified environment file.
+    If no environment file is provided, it defaults to standard parameters defined in the config classes.
+    For error handling, it prints the used environment file path.
+
+    Parameters
+    ----------
+    env_file : Optional[str]
+        The path to the environment file. If None, it uses the default from Settings.
+
+    Returns
+    -------
+    GlobalConfig
+        An instance of GlobalConfig containing all configurations loaded from the environment file.
+
+    """
     if env_file is None:
         settings = Settings()  # Load settings from the .env file
         env_file = settings.env_file
