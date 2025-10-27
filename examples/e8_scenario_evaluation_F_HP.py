eca9bff65d115b10c0c42569f007af8c0bf031ea
jasparpaulus <jasparpaulus@gmail.com>
2025-10-27 11:47:22 +0100

    Scenarios für die Typquartiere

---
+++ examples/e8_scenario_evaluation_F_HP.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+We reached the final step, to generate our first district: Generate demand profiles for our buildings.
+"""
+
+# Import classes of the districtgenerator to be able to use the district generator.
+from districtgenerator.classes import *
+
+
+
+def example8_scenario_evaluation():
+
+    # Initialize District
+    data = Datahandler(scenario_name = "parameters_F_buildings_35_HP_30%PV")
+
+    # We directly generate a complete district.
+    data.generateDistrictComplete(calcUserProfiles=False, saveUserProfiles=False)
+
+    # Calculation of the devices' optimal operation
+    data.optimizationClusters()
+
+    # Calculation of the key performance indicators using the devices' operation profiles of clustered time periods
+    data.calulateKPIs()
+
+    # Create a certificate (PDF) which summarizes the district parameters and calculated KPIs
+    data.KPIs.create_certificate(data=data, result_path=data.resultPath)
+
+    print("Congratulations! You calculated an optimized device operation for the selected neighborhood!")
+
+    return data
+
+
+if __name__ == '__main__':
+    data = example8_scenario_evaluation()
+
