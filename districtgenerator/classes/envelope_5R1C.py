8e9aa7fb528f44f980ed1665364b5417690c4329
Rawad Hamze <rawad.hamze@eonerc.rwth-aachen.de>
2026-02-13 14:49:51 +0100

    fix(envelope): correct internal floor material selection to avoid GroundFloor duplication

--- districtgenerator/classes/envelope_5R1C.py
+++ districtgenerator/classes/envelope_5R1C.py
@@ -337,7 +337,7 @@ class Envelope:
             comp = "intFloor"
             # INTERNAL FLOOR: Materials and U-value
             for name, elem in element_bind.items():
-                if "Floor" in name:
+                if "Floor" in name and "GroundFloor" not in name:
                     dummy = min(2015,
                                 self.construction_year)  # data available until 2015
                     if elem["building_age_group"][0] <= dummy <= \
@@ -642,7 +642,7 @@ class Envelope:
     def calcCoolingLoad(self, site, method="design", nb_occ=2):
         """
         Calculate design (nominal) cooling load at design outside temperature
-        Compare to SIA2024 or VDI2078 for more details on the method.
+        Compare to SIA2024 or VDI2078 for more details about the method.
         https://cms.sia.ch/de/api/getMedia/941
 
         Static calculation pyhsically based on VDI 2078 (1996) and DIN EN ISO 13790
