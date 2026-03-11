4c403a2a375273ece927b8ea99c84dc817d41c89
marvin.kluge <marvin.kluge@eonerc.rwth-aachen.de>
2026-03-11 11:08:06 +0100

    chore: dynamic loading of requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 import setuptools
 
+
+def parse_requirements(filename):
+    with open(filename, encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+
 setuptools.setup(name='districtgenerator',
                  version='0.0.1',
                  description='Energy profile generation and '
@@ -10,21 +16,6 @@ setuptools.setup(name='districtgenerator',
                  author_email='joel.schoelzel@eonerc.rwth-aachen.de',
                  license='MIT License',
                  packages=setuptools.find_packages(),
-                 install_requires=[
-                     'numpy>=1.20.1',
-                     'pandas>=1.2.3',
-                     'matplotlib>=3.3.4',
-                     'scipy>=1.6.1',
-                     'teaser==1.3.0',
-                     'richardsonpy',
-                     'pylightxl',
-                     'gurobipy>=9.5.1',
-                     'xlsxwriter~=3.2.0',
-                     'geopy~=2.4.1',
-                     'pyproj~=3.7.0',
-                     'openpyxl==3.1.5',
-                     'seaborn==0.13.2',
-                     'OpenDHW>=0.2.2'
-                 ],
-                 classifiers=("Programming Language :: Python :: 3", ),
+                 install_requires=parse_requirements('requirements.txt'),
+                 classifiers=("Programming Language :: Python :: 3",),
                  )
