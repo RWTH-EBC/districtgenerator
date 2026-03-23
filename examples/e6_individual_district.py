3b20ba670b9d5b5b5d702b2d84a8286b2597745f
Theresa Janning <theresa.janning@eonerc.rwth-aachen.de>
2026-03-23 16:18:23 +0100

    Add IIS to debug optimization

--- examples/e6_individual_district.py
+++ examples/e6_individual_district.py
@@ -49,7 +49,7 @@ def example6_config_file_loading():
     # That is equal to a time resolution of three hours.
 
     # Initialize District
-    data = Datahandler(env_path=".env.CONFIG.EXAMPLE3", scenario_name = "example3")
+    data = Datahandler(env_path=".env.CONFIG.7ALTSTADT", scenario_name = "7altstadt")
     # Alternative if you need an absolute path:
     # data = Datahandler(env_path="/path/to/your/.env.CONFIG.FREIBURG")
 
