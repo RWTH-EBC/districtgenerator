f63b76cb37688cb44080d600ddb6a3335581b733
Philipp Nimphius <philipp.nimphius@rwth-aachen.de>
2026-06-05 16:45:21 +0200

    Stand zur Simulation

--- districtgenerator/classes/datahandler.py
+++ districtgenerator/classes/datahandler.py
@@ -1806,13 +1806,18 @@ class Datahandler:
 
             # Optionally save PV/STC generation profiles
             if saveGenerationProfiles == True:
-                np.savetxt(os.path.join(self.resultPath, 'generation') + '/decentralPV_' + building["unique_name"] + '.csv',
-                           building["generationPV"],
-                           delimiter=',')
-                np.savetxt(os.path.join(self.resultPath, 'generation')
-                           + '/decentralSTC_' + building["unique_name"] + '.csv',
-                           building["generationSTC"],
-                           delimiter=',')
+                try:
+                    np.savetxt(os.path.join(self.resultPath, 'generation') + '/decentralPV_' + building["unique_name"] + '.csv',
+                            building["generationPV"],
+                            delimiter=',')
+                    np.savetxt(os.path.join(self.resultPath, 'generation')
+                            + '/decentralSTC_' + building["unique_name"] + '.csv',
+                            building["generationSTC"],
+                            delimiter=',')
+                except Exception as e:
+
+                    print(building["generationPV"])
+
 
     def designCentralDevices(self, saveGenerationProfiles):
         """
@@ -2651,12 +2656,12 @@ class Datahandler:
                 )
                 return None
 
-            if row.get("heat_relevance") != "wärmerelevant":
-                print_row_problem(
-                    row_index, alkis_id, "heat_relevance", row.get("heat_relevance"),
-                    "building is not heat-relevant"
-                )
-                return None
+            # if row.get("heat_relevance") != "wärmerelevant":
+            #     print_row_problem(
+            #         row_index, alkis_id, "heat_relevance", row.get("heat_relevance"),
+            #         "building is not heat-relevant"
+            #     )
+            #     return None
 
             building_type = map_building_type(row.get("iwu_class"))
             if building_type is None:
@@ -2690,15 +2695,15 @@ class Datahandler:
                 )
                 return None
 
-            if not has_valid_heat_demand(row.get("heat_demand_simulated"), row.get("energy_consumption_sh")):
-                print_row_problem(
-                    row_index,
-                    alkis_id,
-                    "heat_demand_simulated / energy_consumption_sh",
-                    f"{row.get('heat_demand_simulated')} / {row.get('energy_consumption_sh')}",
-                    "invalid simulated or measured heat demand"
-                )
-                return None
+            # if not has_valid_heat_demand(row.get("heat_demand_simulated"), row.get("energy_consumption_sh")):
+            #     print_row_problem(
+            #         row_index,
+            #         alkis_id,
+            #         "heat_demand_simulated / energy_consumption_sh",
+            #         f"{row.get('heat_demand_simulated')} / {row.get('energy_consumption_sh')}",
+            #         "invalid simulated or measured heat demand"
+            #     )
+            #     return None
 
             return {
                 "alkis_id": alkis_id,
@@ -2795,9 +2800,9 @@ class Datahandler:
 
         # Adjust the nominal_waste_heat_capacity_kW of the heat grid data based on the waste heat potential if it is not already set
         if self.heat_grid_data["nominal_waste_heat_capacity_kW"] is None:
-            key = "Pot_Abwasser_entzugsleistungsbereich_kw"
+            key = "pot_abwasser_entzugsleistungsbereich_kw"
             if key in wkb_data.columns:
-                self.heat_grid_data["nominal_waste_heat_capacity_kW"] = determine_wastewater_heat_potential(wkb_data["Pot_Abwasser_entzugsleistungsbereich_kw"])
+                self.heat_grid_data["nominal_waste_heat_capacity_kW"] = determine_wastewater_heat_potential(wkb_data[key])
             else:
                 self.heat_grid_data["nominal_waste_heat_capacity_kW"] = 0
         return scenario_df
